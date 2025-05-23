183145c1293dc60c4237568f1d14241c41090405
Hermínio Da Silva André Francisco <154982502+eryyoficial@users.noreply.github.com>
2025-05-23 09:21:35 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
+            LinhaSeeder::class,
+            ParagemSeeder::class,
+            AutocarroSeeder::class,
+            HorariosTableSeeder::class,
             AtualizarUsuariosExistentesSeeder::class,
         ]);
     }
